36811933df0bc9827c814c45218fd6b08c6df9c2
dustinos3 <dginos3@hotmail.com>
2016-03-04 10:53:31 -0700

    .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+/*var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Today' });
+});
+
+module.exports = router; */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Today' });
+  res.render('index', { title: 'Blog' });
 });
 
 module.exports = router;
